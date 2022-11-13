7cda7cd160875f7d6b51447aa55db27545b6a129
jcampos302 <59896858+jcampos302@users.noreply.github.com>
2022-11-13 05:05:06 -0600

    It's pretty now

--- virustotal.py
+++ virustotal.py
@@ -3,7 +3,6 @@
 
 # Virus Total API v3 python script to a small generate report
 
-
 import sys
 import requests as requests
 
@@ -17,15 +16,16 @@ headers = {'x-apikey' : APIKEY}
 # Flag match case
 match sys.argv[1]:
     case "-f":
-        temp = "files/"
+        dir_type = "files/"
     case "-u":
-        temp = "urls/"
+        dir_type = "urls/"
     case "-d":
-        temp = "domains/"
+        dir_type = "domains/"
     case "-ip":
-        temp = "ip_addresses/"
+        dir_type = "ip_addresses/"
     case _:
         print("usage: ./virustotal.py [-f: files or hash | -u: urls | -d: domains | -ip: ip address] <object> ")
+        dir_type = None
         exit()
 
 # Checks if object is empty
@@ -34,16 +34,21 @@ if sys.argv[2] == "":
     exit()
 
 # Sends the request
-r = requests.get(url+temp+sys.argv[2], headers=headers)
+r = requests.get(url + dir_type + sys.argv[2], headers=headers)
 
 data = r.json()
 temp = data['data']['attributes']['last_analysis_stats']
 
 # Filters and Print Data
-print("Analysis Statistics")
+print("Names: ")
+for i in data['data']['attributes']['names']:
+    if "." in i:
+        j = i.replace(".", "[.]")
+        print(j)
+print("\nThreat Label: " + str(data['data']['attributes']['popular_threat_classification']['suggested_threat_label']))
+print("\nAnalysis Statistics")
 print("Harmless: " + str(temp['harmless']))
 print("Unsupported: " + str(temp['type-unsupported']))
 print("Suspicious:" + str(temp['suspicious']))
 print("Malicious: " + str(temp['malicious']))
 print("Undetected: " +str(temp['undetected']))
-
